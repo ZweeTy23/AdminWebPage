9cf930a0902d863d2971451dbe8e089bf1557e40
ZweeTy23 <josealbertodc2006@gmail.com>
2025-11-25 21:07:09 -0600

    Controladores Iniciales

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\Admin\SlideController;
+use App\Http\Controllers\FrontController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+//Auth Routes
+Auth::routes();
+Route::group(['prefix'=>'admin','middleware'=>'auth'], function(){
+    Route::resource('/profile', ProfileController::class);
+    Route::resource('/slide', SlideController::class);
 });
 
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//Public Routes
+//Route::get('/', function () {return view('welcome');});
+Route::get('/', [App\Http\Controllers\FrontController::class, 'index']);
+Route::get('/empresa', [App\Http\Controllers\FrontController::class, 'empresa']);
+Route::get('/{category:slug}', [App\Http\Controllers\FrontController::class, 'category']);
+Route::get('/{category.slug}/{product.slug}', [App\Http\Controllers\FrontController::class, 'product']);
+Route::get('/blog', [App\Http\Controllers\FrontController::class, 'blog']);
+Route::get('/blog/{post:slug}', [App\Http\Controllers\FrontController::class, 'post']);
+Route::get('/contacto', [App\Http\Controllers\FrontController::class, 'contacto']);
